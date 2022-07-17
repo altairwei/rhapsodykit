6bf0b37005fa4a4db19f1bdd0e3fe4480c0f2f40
Altair Wei <altair_wei@outlook.com>
2022-07-17 15:51:52 +0800

    bump version

--- R/constants.R
+++
@@ -1,6 +0,0 @@
-# A key for manifest.json, which indicates a list of
-# library result folder produced from Rhapsody WTA pipeline
-KEY_LIB_RESULTS <- "raw_results"
-
-# Result folder produced by Seurat integreted analysis
-KEY_LIB_INTEGRATED <- "integrated"
